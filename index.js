3355b8317bb57228393ca8833e9366533ca97394
mahaultA <mahault.andria@gmail.com>
2023-12-06 22:25:52 +0100

    [ADD] Ajout du comptage du nombre de tentatives

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { prompt } from "./prompt.js";
 
+const displayWelcomeMsg = () => {
+  console.log("Wesh");
+};
+
 const generateRandomNumberBetween0and100 = () => {
   return Math.floor(Math.random() * 100);
 };
@@ -7,34 +11,41 @@ const generateRandomNumberBetween0and100 = () => {
 const promptUserNumber = () => {
   const nbEntered = Number(prompt("Ecris un nombre entre 0 et 100 : "));
   if (Number.isNaN(nbEntered) || nbEntered < 0 || nbEntered > 100) {
+    console.log("Tu n'as pas écrit un nombre valide");
     return promptUserNumber();
   }
   return nbEntered;
 };
 
-const compareTargetNbWithUserNb = (userNb, targetNb) => {
-  if (userNb < targetNb) {
-    console.log("Le nombre à trouver est + grand");
-    let userNumber = promptUserNumber();
-    return compareTargetNbWithUserNb(userNumber, targetNb);
-  }
-  if (userNb > targetNb) {
-    console.log("Le nombre à trouver est + petit");
-    let userNumber = promptUserNumber();
-    return compareTargetNbWithUserNb(userNumber, targetNb);
-  }
-  console.log("BRAVO!");
-  return true;
+const generateTargetNumber = (targetNb) => {
+  let nbAttempts = 0;
+  console.log({ targetNb });
+  const compareUserAnswer = (userNb) => {
+    if (userNb < targetNb) {
+      console.log("Le nombre à trouver est + grand");
+      nbAttempts++;
+      console.log({ nbAttempts });
+      let userNumber = promptUserNumber();
+      return compareUserAnswer(userNumber);
+    }
+    if (userNb > targetNb) {
+      console.log("Le nombre à trouver est + petit");
+      nbAttempts++;
+      console.log({ nbAttempts });
+      let userNumber = promptUserNumber();
+      return compareUserAnswer(userNumber);
+    }
+    nbAttempts++;
+    console.log({ nbAttempts });
+    console.log("BRAVO! Tu as réussi en", nbAttempts, "coup(s)");
+    return true;
+  };
+  console.log({ nbAttempts });
+  return compareUserAnswer;
 };
 
 // Le concept est simple. Au début du script, notre application va générer un nombre aléatoire entre 0 et 100 qu'on va appeler userNumber
-const targetNumber = generateRandomNumberBetween0and100();
+const targetNumber = generateTargetNumber(generateRandomNumberBetween0and100());
 
-//     Si le nombre n'est pas valide, il informe qu'il faut rentrer un nombre entre 0 et 100
 let userNumber = promptUserNumber();
-
-//     Si le nombre est plus petit, il va informer que le nombre userNumber est plus grand
-//     Si le nombre est trop grand, il va informer que le nombre userNumber est plus petit
-
-compareTargetNbWithUserNb(userNumber, targetNumber);
-//     Si c'est le bon nombre : fin de la partie ! (affiche le nombre d'essais)
+const nbEntreParUser = targetNumber(userNumber);
